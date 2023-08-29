a7b6eed0c2ce248b1df086ce1c71712d86e6ad58
ziupan <chenzhoufan@chiefood.com>
2023-08-29 17:23:31 +0800

    Initial commit

---
+++ src/test/java/com/example/parameter_check_learn/ParameterCheckLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parameter_check_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterCheckLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
